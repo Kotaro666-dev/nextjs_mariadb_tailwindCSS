8d4808708b084904afbf22b3f09b31b2caf8caf4
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-11 13:11:01 +0900

    Update: Use ListCard and add page transition with query

--- srcs/pages/category_0/details/[id].tsx
+++ srcs/pages/category_0/details/[id].tsx
@@ -1,24 +1,22 @@
 import type { NextPage } from 'next'
-import { FilledButton, WhiteButton } from '../../components'
+import { FilledButton, WhiteButton } from '../../../components'
 import { useRouter } from 'next/router'
-
 
 const Details: NextPage = () => {
     const router = useRouter()
+    const query = router.query
 
-    const handleEditButtonOnClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        e.preventDefault()
+    const handleEditButtonOnClick = () => {
         router.push('./edit')
     }
 
-    const handleBackButtonOnClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-        e.preventDefault()
-        router.replace('./')
+    const handleBackButtonOnClick = () => {
+        router.back()
     }
 
   return (
     <>
-     <h1 className='text-4xl'>詳細</h1>
+     <h1 className='text-4xl'>詳細 - {query.title}</h1>
         <div className='flex justify-end'>
             <WhiteButton title='戻る' onClick={handleBackButtonOnClick}/>
             <FilledButton title='編集' onClick={handleEditButtonOnClick}/>
--- srcs/pages/category_0/index.tsx
+++ srcs/pages/category_0/index.tsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import type { NextPage } from 'next'
-import { DataEmptyBanner, FilledButton } from '../../components'
+import { DataEmptyBanner, FilledButton, ListCard } from '../../components'
 import { useRouter } from 'next/router'
+
+export interface Card {
+  id: number,
+  title: string,
+}
+
+const items: Card[] = [
+  { id: 0, title: 'テスト0' },
+  { id: 1, title: 'テスト1' },
+  { id: 2, title: 'テスト2' },
+  { id: 3, title: 'テスト3' },
+  { id: 4, title: 'テスト4' },
+  { id: 5, title: 'テスト5' },
+  { id: 6, title: 'テスト6' },
+  { id: 7, title: 'テスト7' },
+  { id: 8, title: 'テスト8' },
+  { id: 9, title: 'テスト9' },
+]
 
 
 const Category_0: NextPage = () => {
   const router = useRouter()
-  const [isData, setIsData] = useState<boolean>(false);
+  const [isData, setIsData] = useState<boolean>(true);
 
   const handleOnClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
       e.preventDefault()
@@ -23,7 +41,7 @@ const Category_0: NextPage = () => {
     } else {
       return (
         <div  className='mt-10'>
-          <p>データがあります</p>
+          <ListCard items={items}/>
         </div>
       )
     }
