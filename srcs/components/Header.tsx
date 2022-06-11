8be495388445ef09ceed817c1c92ddfb6ae7dbc6
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-11 11:29:22 +0900

    Modify: Modify routing

--- srcs/components/Header.tsx
+++ srcs/components/Header.tsx
@@ -1,8 +1,7 @@
 const Header = () => {
     return (
       <header className="bg-indigo-600 w-full h-20 flex items-center pl-5">
-        <a href="#">
-            <span className="sr-only">Workflow</span>
+        <a href="./">
             <img
                 className="h-10 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
