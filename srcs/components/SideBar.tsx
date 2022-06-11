818f1b687412207fb99c26362fa9f58adc38bdb2
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-11 09:09:47 +0900

    Update: Add Sidebar

---
+++ srcs/components/SideBar.tsx
@@ -0,0 +1,44 @@
+import {
+  CalendarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+} from '@heroicons/react/outline'
+
+interface Category {
+  name: string,
+  href: string,
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>,
+}
+
+const items: Category[] = [
+  { name: 'カテゴリー0', href: '/', icon: HomeIcon, },
+  { name: 'カテゴリー1', href: '/category_1', icon: UsersIcon },
+  { name: 'カテゴリー2', href: '/category_2', icon: FolderIcon },
+  { name: 'カテゴリー3', href: '/category_3', icon: CalendarIcon },
+  { name: 'カテゴリー4', href: '/category_4', icon: InboxIcon },
+]
+
+const SideBar = () => {
+  return (
+    <div className="hidden md:flex md:w-64">
+      <div className="flex-1 h-screen flex flex-col min-h-0 bg-indigo-700">
+          <nav className="mt-5 flex-1 px-2 space-y-5">
+            {items.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="group flex items-center px-3 py-3 text-sm text-white font-medium rounded-md"
+              >
+                <item.icon className="mr-3 flex-shrink-0 h-8 w-8 text-indigo-300" aria-hidden="true" />
+                {item.name}
+            </a>
+            ))}
+          </nav>
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
--- srcs/components/index.tsx
+++ srcs/components/index.tsx
@@ -2,3 +2,4 @@
 // https://zenn.dev/longbridge/articles/dc0f576b377467
 
 export { default as Header } from './Header'
+export { default as SideBar } from './SideBar'
