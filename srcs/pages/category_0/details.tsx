830aa5b85e5bbaeb9ab3578c850b814cd555d31b
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-11 11:30:31 +0900

    Update: Add details and routing

---
+++ srcs/pages/category_0/details.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Details: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+     Details
+    </h1>
+  )
+}
+
+export default Details
--- srcs/pages/category_0/index.tsx
+++ srcs/pages/category_0/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next'
-import { Content } from '../../components'
+import { FilledButton } from '../../components'
+import { useRouter } from 'next/router'
+
 
 const Category_0: NextPage = () => {
+  const router = useRouter()
+
+  const handleOnClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+      e.preventDefault()
+      router.push('./category_0/details')
+  }
+
   return (
-    <div>
-      <Content title='タイトル1' body={<div>これは本文です</div>}/>
+    <div >
+      <FilledButton title='詳細画面' onClick={handleOnClick}/>
     </div>
   )
 }
