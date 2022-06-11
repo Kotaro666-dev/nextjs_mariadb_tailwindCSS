5b222087c4f285bd103549a059abf9635fae5a7c
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-11 15:36:57 +0900

    Modfiy: Enable ListCard to push dynamic path

--- srcs/components/ListCard.tsx
+++ srcs/components/ListCard.tsx
@@ -1,16 +1,16 @@
 import { Card } from "../pages/category_0"
 import { useRouter } from 'next/router'
 
-const ListCard: React.FC<{ items: Card[]}> = (props) => {
+const ListCard: React.FC<{ items: Card[] }> = (props) => {
     const router = useRouter()
+    const basePath = router.pathname
 
     const handleItemOnClick = (card: Card) => {
         router.push({
-            pathname: './category_0/details/[id]',
+            pathname: `${basePath}/details/[id]`,
             query: { id: card.id, title: card.title},
         })
       }
-
 
     return (
         <div className="bg-white overflow-hidden rounded-md border-2 border-gray-300">
