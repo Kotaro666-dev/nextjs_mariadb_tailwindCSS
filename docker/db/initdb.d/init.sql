2103cea3b7f61d183c8ece979e30f2f651a0d482
Kotaro Kamashima <kotaro0726@gmail.com>
2022-06-18 00:49:40 +0900

    Update: Add init.sql and start this sql when docker launches

---
+++ docker/db/initdb.d/init.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS Category_1;
+CREATE TABLE Category_1 (
+    id         INT(20) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title       VARCHAR(50) NOT NULL
+);
+
+INSERT INTO category_1 (id, title) VALUES (NULL, 'テスト0');
+INSERT INTO category_1 (id, title) VALUES (NULL, 'テスト1');
+INSERT INTO category_1 (id, title) VALUES (NULL, 'テスト2');
+INSERT INTO category_1 (id, title) VALUES (NULL, 'テスト3');
+INSERT INTO category_1 (id, title) VALUES (NULL, 'テスト4');
